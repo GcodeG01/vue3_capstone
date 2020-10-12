c278b4fdaead29dcb9deca3290d815d557e673f2
GcodeG01 <52094879+GcodeG01@users.noreply.github.com>
2020-10-12 11:06:24 -0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,52 @@
+<template>
+  <Layout>
+    <template v-slot:header>
+      Header
+    </template>
+
+    <template v-slot:sidebar>
+      <div v-for="album in albums">
+        {{ album.title }}
+      </div>
+      Sidebar
+    </template>
+
+    <template v-slot:content>
+      Content
+    </template>
+  </Layout>
+</template>
+
+<script>
+import Layout from './Layout.vue'
+
+export default {
+  components: {
+    Layout
+  },
+
+  async created() { // fetching when first load
+    this.$store.dispatch('albums/fetchAlbums')
+  },
+
+  mounted() {
+     console.log(this.$store.state.albums.all)
+  },
+
+  computed: {
+    albums() {
+      return this.$store.state.albums.all
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+</style>
